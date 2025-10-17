605f009b3f0e840415418560da2bb948e3ccdab9
Alexander Nenakhov <a.nenahov@bftcom.com>
2025-10-18 01:55:08 +0300

    Переписывает обработчики ESC

--- js/forms/form-photo-upload.js
+++ js/forms/form-photo-upload.js
@@ -5,7 +5,7 @@ const imgUpload = {
   defaultURL: 'img/upload-default-image.jpg',
   form: imgUploadContainer.querySelector('#upload-select-image'),
   control: imgUploadContainer.querySelector('#upload-file'),
-  modal: imgUploadContainer.querySelector('.img-upload__overlay'),
+  overlay: imgUploadContainer.querySelector('.img-upload__overlay'),
   buttonCloseModal: imgUploadContainer.querySelector('#upload-cancel'),
   preview: imgUploadContainer.querySelector('.img-upload__preview img'),
   previewEffectThumbnails: imgUploadContainer.querySelectorAll('.effects__preview')
@@ -15,9 +15,23 @@ const imgUploadButtonCloseModalClickHandler = () => {
   closeModal();
 };
 
+const imgUploadOverlayClickHandler = (evt) => {
+  if (evt.target.classList.contains('img-upload__overlay')) {
+    closeModal();
+  }
+};
+
+const EscPressHandler = (evt)=> {
+  if (evt.key === 'Escape') {
+    imgUpload.overlay.classList.add('hidden');
+  }
+};
+
 function closeModal() {
-  imgUpload.modal.classList.add('hidden');
+  imgUpload.overlay.classList.add('hidden');
   body.classList.remove('modal-open');
+  imgUpload.buttonCloseModal.removeEventListener('click', imgUploadButtonCloseModalClickHandler);
+  imgUpload.overlay.removeEventListener('click', imgUploadOverlayClickHandler);
 }
 
 const getFileURL = ()=> {
@@ -27,7 +41,7 @@ const getFileURL = ()=> {
 };
 
 const imgUploadControlChangeHandler = ()=> {
-  imgUpload.modal.classList.remove('hidden');
+  imgUpload.overlay.classList.remove('hidden');
   body.classList.add('modal-open');
   imgUpload.preview.src = `${ getFileURL() }`;
   imgUpload.previewEffectThumbnails.forEach((thumbnail)=> {
@@ -35,8 +49,10 @@ const imgUploadControlChangeHandler = ()=> {
   });
 
   imgUpload.buttonCloseModal.addEventListener('click', imgUploadButtonCloseModalClickHandler, { once: true });
+  imgUpload.overlay.addEventListener('click', imgUploadOverlayClickHandler);
 };
 
+document.addEventListener('keydown', EscPressHandler);
 imgUpload.control.addEventListener('change', imgUploadControlChangeHandler);
 
 export { imgUploadControlChangeHandler };
--- js/render-big-photo.js
+++ js/render-big-photo.js
@@ -1,4 +1,4 @@
-import { body, createEscHandler, removeEscHandler } from './utils.js';
+import { body } from './utils.js';
 
 const COMMENTS_SHOWN = 5;
 const bigPictureContainer = document.querySelector('.big-picture');
@@ -37,10 +37,19 @@ const buttonCloseModalClickHandler = () => {
   closeModal();
 };
 
+const EscPressHandler = (evt)=> {
+  if (evt.key === 'Escape') {
+    bigPictureContainer.classList.add('hidden');
+    buttonCloseModal.removeEventListener('click', buttonCloseModalClickHandler);
+    bigPictureContainer.removeEventListener('click', bigPictureContainerClickHandler);
+    buttonLoadMore.removeEventListener('click', buttonLoadMoreClickHandler);
+    currentDisplayedCount = COMMENTS_SHOWN;
+  }
+};
+
 function closeModal() {
   bigPictureContainer.classList.add('hidden');
   body.classList.remove('modal-open');
-  removeEscHandler();
   bigPictureContainer.removeEventListener('click', bigPictureContainerClickHandler);
   buttonLoadMore.removeEventListener('click', buttonLoadMoreClickHandler);
   currentDisplayedCount = COMMENTS_SHOWN;
@@ -91,15 +100,11 @@ const thumbnailClickHandler = (data)=> function (evt) {
     }
   }
 
-  createEscHandler(bigPictureContainer, 'hidden', ()=> {
-    buttonCloseModal.removeEventListener('click', buttonCloseModalClickHandler);
-    bigPictureContainer.removeEventListener('click', bigPictureContainerClickHandler);
-    buttonLoadMore.removeEventListener('click', buttonLoadMoreClickHandler);
-    currentDisplayedCount = COMMENTS_SHOWN;
-  });
   buttonCloseModal.addEventListener('click', buttonCloseModalClickHandler, { once: true });
   bigPictureContainer.addEventListener('click', bigPictureContainerClickHandler);
   buttonLoadMore.addEventListener('click', buttonLoadMoreClickHandler);
 };
+
+document.addEventListener('keydown', EscPressHandler);
 
 export { thumbnailClickHandler };
