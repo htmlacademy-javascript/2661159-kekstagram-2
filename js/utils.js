864ef5b012fa2661ae1d89dc72cf2dffebc121be
Alexander Nenakhov <a.nenahov@bftcom.com>
2025-10-27 23:59:35 +0300

    Добавляет функцию склонения 'pluralize'

--- js/utils.js
+++ js/utils.js
@@ -32,9 +32,30 @@ const createUniqueValueFromRange = (min, max) => {
 // Перемешивание массива
 const shuffleArray = (array)=> array.sort(()=> Math.random() - 0.5);
 
+// Склонение слова после числа
+const pluralize = (num, declensions) => {
+  const n = Math.abs(num) % 100;
+  const n1 = n % 10;
+
+  if (n > 10 && n < 20) {
+    return declensions[2];
+  }
+
+  if (n1 > 1 && n1 < 5) {
+    return declensions[1];
+  }
+
+  if (n1 === 1) {
+    return declensions[0];
+  }
+
+  return declensions[2];
+};
+
 export {
   body,
   getRandomPositiveInteger,
   createUniqueValueFromRange,
-  shuffleArray
+  shuffleArray,
+  pluralize
 };
